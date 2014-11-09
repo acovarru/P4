4329bf0c2b52ee29969bd98cf30d151b6e171bc9
acovarru <acovarru@live.com>
2014-11-09 13:12:05 -0800

    bound post message

--- app/routes.php
+++ app/routes.php
@@ -24,7 +24,24 @@ Route::get('/', function()
  
 Route::get('/message', function()
 {
-	return View::make('sendmessage');
+    $tests = Test::all();
+
+    # Make sure we have results before trying to print them...
+    if($tests->isEmpty() != TRUE) {
+
+        # Typically we'd pass $books to a View, but for quick and dirty demonstration, let's just output here...
+        foreach($tests as $test) {
+            
+            echo $test->message.'<br>';
+            echo ''.'<br>';
+            echo 'sent on:'.$test->created_at.'<br>';
+            
+        }
+    }
+    else {
+        return 'No message found';
+    }
+	return View::make('/sendmessage');
 });
 
 
@@ -74,7 +91,28 @@ Route::get('/sendmessagepost', function()
     else {
         return 'No message found';
     }
-return View::make('sendmessagepost');
+return View::make('/sendmessage');
+});
+
+
+Route::post('/sendmessagepost', function()
+{
+
+ //$tests = Test::all();
+	 # Instantiate a new Test model class
+    $test = new Test();
+
+    # Set 
+    # this can be set later to have a conversation name, create conversations or chats
+   // $test->conversation = $_POST['conversation'];
+    $test->message = $_POST['message'];
+   
+
+    # This is where the Eloquent ORM magic happens
+    $test->save();
+
+    //echo 'message saved on db';
+    return Redirect::to('/sendmessagepost');
 });
 
 
